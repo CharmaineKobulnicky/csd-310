17cce04c021c82183f52be751e65e57ce07cbfae
CharmaineKobulnicky <shanewue@gmail.com>
2022-07-02 02:45:30 -0500

    Started project.

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pymongo import insert_one
 
 MongoDB: insert_one()
@@ -7,13 +6,17 @@ students = {
 }
 
 MongoDB: insert_one()
-student_id = {
-    "student_id": "1007, 1008, 1009"
+students = {
+    "student_id": "1007",
+},
+{
+    "student_id": "1008",
+},
+{
+    "student_id": "1009"
 }
 
 charmaine_student_id = students.insert_one(1007, 1008, 1009).inserted_id
 
 print (charmaine_student_id)
-=======
 
->>>>>>> 9f2a286d7fe57571282005b1b4ace9f7bea01a78
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,10 @@
+MongoDB: find()
+docs = db.collection_students.find({})
 
+for doc in docs:
+print(doc)
+
+MongoDB: students.find_one()
+doc = db.collection_students.find_one({"student_id": "1007"})
+doc = db.collection_students.find_one({"student_id": "1008"})
+doc = db.collection_students.find_one({"student_id": "1009"})
