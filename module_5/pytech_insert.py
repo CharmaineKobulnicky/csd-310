0a2d86202635e93e94ae76bd3587b678702b6ec7
CharmaineKobulnicky <shanewue@gmail.com>
2022-07-04 00:30:13 -0500

    pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,13 +1,37 @@
+import pymongo
+from pymongo import MongoClient
+from pymongo import MongoDB
 from pymongo import insert_one
+from pymongo import students
+
+client = pymongo.MongoClient ("mongodb+srv://admin:admin@cluster0.lej4x8e.mongodb.net/?retryWrites=true&w=majority")
+db = client ["pytech"]
+collection = db ["students"]
 
 MongoDB: insert_one()
-students = {
-"first_name": "Charmaine"
-}
-MongoDB: insert_one()
+fred = {
+    "first_name": "Fred"
+},
 student_id = {
-    "student_id": "1007, 1008, 1009"
+    "student_id": "1007"
 }
-charmaine_student_id = students.insert_one(1007, 1008, 1009).inserted_id
+jake = {
+    "first_name": "Jake"
+},
+student_id = {
+    "student_id": "1008"
+}
+hanz = {
+    "first_name": "Hanz"
+},
+student_id = {
+    "student_id": "1009"
+}
 
-print (charmaine_student_id)
+fred_student_id = students.insert_one(fred).inserted_id
+jake_student_id = students.insert_one(jake).inserted_id
+hanz_student_id = students.insert_one(hanz).inserted_id
+
+print(fred_student_id)
+print(jake_student_id)
+print(hanz_student_id)
