8588a4b9f5c0881ff3bdf4880395ae11a3079b4f
CharmaineKobulnicky <shanewue@gmail.com>
2022-07-08 16:55:25 -0500

    pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,37 +1,20 @@
 import pymongo
 from pymongo import MongoClient
-from pymongo import MongoDB
-from pymongo import insert_one
-from pymongo import students
 
-client = pymongo.MongoClient ("mongodb+srv://admin:admin@cluster0.lej4x8e.mongodb.net/?retryWrites=true&w=majority")
-db = client ["pytech"]
-collection = db ["students"]
+url = "mongodb+srv://admin:admin@cluster0.lej4x8e.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client["pytech"]
+collection = db["students"]
 
-MongoDB: insert_one()
-fred = {
-    "first_name": "Fred"
-},
-student_id = {
-    "student_id": "1007"
-}
-jake = {
-    "first_name": "Jake"
-},
-student_id = {
-    "student_id": "1008"
-}
-hanz = {
-    "first_name": "Hanz"
-},
-student_id = {
-    "student_id": "1009"
-}
 
-fred_student_id = students.insert_one(fred).inserted_id
-jake_student_id = students.insert_one(jake).inserted_id
-hanz_student_id = students.insert_one(hanz).inserted_id
+fred = {"first_name": "Fred", "last_name": "Bras", "student_id": "1007"}
+jake = {"first_name": "Jake", "last_name": "Brown", "student_id": "1008"}
+hanz = {"first_name": "Hanz", "last_name": "Harris", "student_id": "1009"}
 
-print(fred_student_id)
-print(jake_student_id)
-print(hanz_student_id)
+print("-- INSERT STATEMENT --")
+fred_student_id = collection.insert_one(fred).inserted_id
+print(f"Inserted student record Fred Bras into the students collection with document id {fred_student_id}")
+jake_student_id = collection.insert_one(jake).inserted_id
+print(f"Inserted student record Jake Brown into the students collection with document id {jake_student_id}")
+hanz_student_id = collection.insert_one(hanz).inserted_id
+print(f"Inserted student record Hanz Harris into the students collection with document id {hanz_student_id}")
