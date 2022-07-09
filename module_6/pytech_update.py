c5f43d0c19f3cf1e0b63d61fc37c23757b7fbf82
CharmaineKobulnicky <shanewue@gmail.com>
2022-07-09 02:34:13 -0500

    pytech_update.py

---
+++ module_6/pytech_update.py
@@ -0,0 +1,37 @@
+"""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.lej4x8e.mongodb.net/?retryWrites=true&w=majority"
+
+# connect to the MongoDB cluster
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# get the students collection
+students = db.students
+
+# find all students in the collection
+student_list = students.find({})
+
+# display message
+print("\n -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+# loop over the collection and output the results
+for doc in student_list:
+    print(" Student ID: " + doc["student_id"] + "\n First Name: " + doc["first_name"] + "\n Last Name: " +doc["last_name"] + "\n")
+
+# update student_id 1007
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Brass II"}})
+
+# find the updated student document
+fred = students.find_one({"student_id": "1007"})
+
+# display message
+print("\n -- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+# output the updated document to the terminal window
+print(" Student ID: " + fred["student_id"] + "\n First Name : " + fred["first_name"] + "\n Last Name: " + fred["last_name"] + "\n")
+
